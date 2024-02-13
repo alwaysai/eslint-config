f2fbdf73b37b4cebfa316f8a83696156877fdcb1
Lila Mullany <lilamullany@gmail.com>
2024-02-12 21:48:06 -0500

    Update rules and include required tsconfig changes

--- src/example.ts
+++ src/example.ts
@@ -1,10 +1,12 @@
 // This is just a dummy .ts file to play with lint rules and
 // test that the root .eslintrc.json is working as expected.
 
-export class Foo {
+class Foo {
   public bar: string;
 
   public constructor() {
     this.bar = 'bar';
   }
 }
+
+export default Foo;
